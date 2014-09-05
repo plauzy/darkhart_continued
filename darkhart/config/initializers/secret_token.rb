3d46343724d7886fcb387b902351cb4e498ef501
Cassidy Clawson & Patrick Lauer <pair+cassidyclawson.plauer@devbootcamp.com>
2014-09-04 17:11:08 -0700

    Initial commit ... - New rails projects with rails-api gem. - Added puma Procfile for Heroku - Demonstration index.html - Runs.

---
+++ darkhart/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Darkhart::Application.config.secret_token = 'f34c75abe1f735e1fcc9dc5bcb71b642cc052368378a8901e55a78c8427c0177417da73c16b403687b632f95417dae77b573fed182208f71b643afe89af86c9b'
