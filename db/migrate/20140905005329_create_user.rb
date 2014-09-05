31fd58010b83c1754b78667e365ccf6f09f06b2f
Cassidy Clawson & Patrick Lauer <pair+cassidyclawson.plauer@devbootcamp.com>
2014-09-04 22:36:43 -0700

    removes game_id from user field since its many to many through scores now

--- db/migrate/20140905005329_create_user.rb
+++ db/migrate/20140905005329_create_user.rb
@@ -1,7 +1,6 @@
 class CreateUser < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.integer :game_id
       t.string :name, null: false, limit:30
       t.string :email
       t.string :password
