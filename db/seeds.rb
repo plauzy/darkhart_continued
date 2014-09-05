38b7867a64e014a57d9d498b002ea15e38098c69
Ian Bui <thinkbui@yahoo.com>
2014-09-04 21:10:09 -0700

    Created working seed files printing to console pending database creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Code retrieved and modified from https://github.com/nodanaonlyzuul/against-humanity
+
+require 'json'
+
+cards = JSON.parse(File.read("./db/third_party/cards.json"))
+
+questions, answers = cards.partition{ |card| card["cardType"] == "Q"}
+
+# File.open("questions.txt", "w") do |f|
+#   questions.reject!{ |card| card["numAnswers"]>1 }
+#   questions.each do |question|
+#     f << "#{question["text"]}\n"
+#   end
+# end
+
+# File.open("answers.txt", "w") do |f|
+#   answers.each do |answer|
+#     f << "#{answer["text"]}\n"
+#   end
+# end
+
+questions.each do |question|
+  # BlackCard.create(content:question["text"]) unless question["numAnswers"]>1
+  p question["text"] unless question["numAnswers"] > 1
+end
+
+answers.each do |answer|
+  # WhiteCard.create(content:answer["text"])
+  p answer["text"]
+end
