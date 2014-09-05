5085eeea1c2c4fb1296de2448d767e15c8ce0957
Cassidy Clawson & Patrick Lauer <pair+cassidyclawson.plauer@devbootcamp.com>
2014-09-04 23:07:34 -0700

    removes plural reference to submission in submissions model file and migration files

--- db/migrate/20140905042020_create_submissions.rb
+++
@@ -1,9 +0,0 @@
-class CreateSubmissions < ActiveRecord::Migration
-  def change
-    create_table :submissions do |t|
-      t.integer :round_id
-      t.integer :cards_in_play_id
-      t.boolean :winner, default: false
-    end
-  end
-end
