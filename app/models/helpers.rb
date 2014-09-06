dc04f01793e07c857b23d0603fe58ab40f4666c8
Brooks Roley <brooksroley@gmail.com>
2014-09-06 16:08:57 -0700

    adds helper method files and two methods

---
+++ app/models/helpers.rb
@@ -0,0 +1,9 @@
+  def random_whitecard #add validation to prevent duplicate cards and offset is a valid Whitecard
+    offset = rand(1..(Whitecard.count))
+    Whitecard.find(offset)
+  end
+
+  def random_blackcard #add validation to prevent duplicate cards and offset is valid blackcard
+    offset = rand(1..(Blackcard.count))
+    Blackcard.find_by_id(offset)
+  end
