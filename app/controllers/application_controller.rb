f7cf03ff683ee3eef4a6af2a6fdd86bf0ca15d51
Cassidy Clawson & Ian Bui <pair+cassidyclawson.thinkbui@devbootcamp.com>
2014-09-06 15:36:12 -0700

    adds a couple methods to application ctroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::API
   respond_to :json
-  def get_round
+
+  #post '/api/games/:game_id'
+  def new_game
+    #create new game, start it at round 1
+    player_ids = params[:players]
+    game = GameManager::GameSetup.new(params[:game_name], player_ids)
+
+
+  end
+
+  #get '/api/games/:game_id/rounds/:id'
+  def get_completed_round
     #if round is complete, return round recap and id of current round.
     #elsif round is in progress, return data necessary for card submission:
       #if user is leader && all white cards submitted, returns black card and white cards to be evaluated.
@@ -9,8 +20,18 @@ class ApplicationController < ActionController::API
       #else player has already submitted a card, return error message.
     render json: {content:"Hello World"}
   end
+
+  #get '/api/games/:game_id/rounds/current'
+  def get_current_round
+
+  end
+
   def submit_card
     #if user is leader, recieve their chosen whitecard.
     #elsif user is a player, recive their whitecard for consideration by leader.
   end
+
+
+
+
 end
