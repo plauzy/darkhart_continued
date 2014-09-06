03a58fb7cae2383e1b0fe2b411d47145b55afb18
Cassidy Clawson & Patrick Lauer <pair+cassidyclawson.plauer@devbootcamp.com>
2014-09-05 20:06:45 -0700

    fixed placeholder action route error, routes still not functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get '/api/users/:user_id' => 'application#' # List of all games user is in.
-  post '/api/users/:user_id' => 'application#' # Create a new user
+  get '/api/users/:user_id' => 'application#xxx' # List of all games user is in.
+  post '/api/users/:user_id' => 'application#xxx' # Create a new user
 
   get '/api/games/:game_id' => 'application#active_games'# Returns status of a game
-  post '/api/games/:game_id' => 'application#' # Creates new game
+  post '/api/games/:game_id' => 'application#xxx' # Creates new game
 
   get '/api/games/:game_id/rounds/:round_id' => 'application#get_round' # Round data
   put '/api/games/:game_id/rounds/:round_id' => 'application#submit_card' # Send in chosen card
