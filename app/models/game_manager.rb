6565323f4d5fe23b103078d01f674d6477bd17b0
Cassidy Clawson & Patrick Lauer <pair+cassidyclawson.plauer@devbootcamp.com>
2014-09-06 00:17:34 -0700

    controller creates game with seats, playable cards and rounds

--- app/models/game_manager.rb
+++ app/models/game_manager.rb
@@ -10,10 +10,12 @@ module GameManager
     game_users = user_ids.map { |user_id| user = User.find_by_id(user_id) }
     populate_seats(new_game, game_users)
     deal_cards_to_seats(new_game)
+    make_rounds(new_game, 10) #hardcoded. 10, punted for mVP.
+
     return new_game
   end
 
-  # private
+  private
 
   def populate_seats(new_game, game_users)
     game_users.each do |user|
@@ -22,11 +24,24 @@ module GameManager
     end
   end
 
-  def deal_cards_to_seats(seats)
-    seats.each do |seat|
+  def deal_cards_to_seats(new_game)
+    new_game.seats.each do |seat|
       10.times do
-        PlayableCard.create(seat_id: seat.id, whitecard_id: random_whitecard)
+        PlayableCard.create(seat_id: seat.id, whitecard_id: random_whitecard.id)
       end
+    end
+  end
+
+  def make_rounds(new_game, number_of_rounds)
+    user_array = new_game.seats.to_a
+    for round_num in (1..number_of_rounds)
+      new_round = Round.create( game_id: new_game.id,
+                                leader_id: user_array.first,
+                                blackcard_id: random_blackcard.id,
+                                round_num: round_num)
+      new_game.rounds << new_round
+      new_game.save
+      user_array.rotate!
     end
   end
 
