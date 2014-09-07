752e2d3d4e5fb5a936d6dcf8490b02b341df1ad8
Cassidy Clawson & Patrick Lauer <pair+cassidyclawson.plauer@devbootcamp.com>
2014-09-06 22:19:55 -0700

    adds model methods to easily grab a submitted cards owner_seat, card content, owner_name, and owner_score

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,7 +2,19 @@ class Submission < ActiveRecord::Base
   belongs_to :round
   belongs_to :playable_card
 
-  def owner
+  def owner_seat
     self.playable_card.seat
+  end
+
+  def card_content
+    self.playable_card.whitecard.content
+  end
+
+  def owner_name
+    self.playable_card.seat.user.name
+  end
+
+  def owner_score
+    self.playable_card.seat.score
   end
 end
