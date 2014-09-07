e8718595ad467733b128d9fd3e503561451c4daa
Cassidy Clawson <cassidyclawson@gmail.com>
2014-09-07 15:01:04 -0700

    implements setter route as get temporarily

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::API
 
   def game_state
     round = params["round"] || 0
-    p "------------- #{round}"
     game = GameGetter.new(params["game_id"], params["user_id"], round.to_i)
     render :json => game.game_state
   end
 
+  def submit_card
+    GameSetter.new(params["game_id"],params["user_id"],params["card_id"]).submit_card
+    game = GameGetter.new(params["game_id"], params["user_id"])
+    render :json => game.game_state
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'api/users/:user_id' => 'application#user_state' # Returns all games the user is in.
-  get 'api/users/:user_id/games/:game_id' => 'application#game_state'
-  get 'api/users/:user_id/games/:game_id/rounds/:round' => 'application#game_state' # Returns state of a game
 
-  post 'api/games' => 'application#new_game' # Creates new game and returns game_state
-  post 'api/games/:game_id/users/:user_id/cards/:card_id' => 'application#update_game' #Submits a card
+  get 'api/users/:user_id' => 'application#user_state' # Returns all games the user is in.
+  post 'api/users/:user_id/games' => 'application#new_game' # Creates new game and returns game_state
+
+  get 'api/users/:user_id/games/:game_id' => 'application#game_state'
+  get 'api/users/:user_id/games/:game_id/rounds/:round' => 'application#game_state'
+
+  get 'api/users/:user_id/games/:game_id/cards/:card_id' => 'application#submit_card' # GET FOR TESTING ONLY
 end
