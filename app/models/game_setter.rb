9721709df2097bac3537790ff993110c3bc9da61
plauer <plauer411@gmail.com>
2014-09-07 11:58:39 -0700

    snapshot of game_stter before pull from mastera

--- app/models/game_setter.rb
+++ app/models/game_setter.rb
@@ -3,20 +3,26 @@ class GameSetter
   def initialize(seat_id)
     @seat = Seat.find_by_id(seat_id)
     @game = @seat.game
-    @current_round = @game.current_round
+    @round_num = @game.current_round
+    @round = Round.where(["round_num = ? and game_id = ?", @game.current_round, @game.id )
+    @state = {}
   end
 
   def make_user_submission(playable_card)
-    submission = Submission.create(playable_card_id: playable_card.id, round_id: @round.id) unless playable_card.submitted == true
+    @submission = Submission.create(playable_card_id: playable_card.id, round_id: @round.id) unless playable_card.submitted == true
     playable_card.submitted = true 
     playable_card.save!
   end
 
-  def make_rounder_leader_decision(winning_submission)
+  def choose_winner(winning_submission)
     winning_submission.winner = true
     winning_submission.save!
+
     increment_round 
+    deal_new_card_to_seats
   end
+
+  private
 
   def increment_round 
     @game.current_round+=1
@@ -30,10 +36,47 @@ class GameSetter
     end
   end
 
+  def round_active?
+    @submissions.where(winner: true)[0] ? false : true
+  end
+
+  def build_header
+    @state["round"] = @round.round_num
+    @state["active"] = round_active?
+    @state["player_self"] = player_self
+    @state["leader"] = leader_blackcard
+    @state["need_submission?"] = need_submission?
+  end
+
+
+  # def build_player_submission
+  #   @state["authentication"] = @seat.user.id
+  #   @seat["game_id"] = @game.id
+  #   @seat["submission_id"] = @submission.id
+  # end
+
+  # def build_leader_submission 
+  #   @state["authentication"] = @seat.user.id
+  #   @seat["game_id"] = @game.id
+  #   @seat
+  # end
+
+
+
+
+
 end
 
+{authentication: [user_id],
+game_id: 1,
+submission_id: 1}
+
+{authentication: [user_id],
+game_id: 1, 
+playable_card_id: 1}
 
 
+__END__
 
 
 
