931616e2fdacfb4c8c9db298e4fad19558c57bb1
Cassidy Clawson & Patrick Lauer <pair+cassidyclawson.plauer@devbootcamp.com>
2014-09-06 20:05:55 -0700

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140906234340) do
+ActiveRecord::Schema.define(version: 20140907014713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140906234340) do
   create_table "seats", force: true do |t|
     t.integer "user_id"
     t.integer "game_id"
+    t.integer "score",   default: 0
   end
 
   create_table "submissions", force: true do |t|
