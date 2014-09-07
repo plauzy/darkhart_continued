3fac25ec2f2669fd71f24ac9c1e96c9370d1fe3e
Brooks Roley <brooksroley@gmail.com>
2014-09-06 17:24:49 -0700

    snapshot of class methods progress

--- app/models/game_getter.rb
+++ app/models/game_getter.rb
@@ -1,8 +1,15 @@
 class GameGetter
+
+  def initialize(seat_id)
+    @seat = Seat.find_by_id(seat_id)
+    @game = @seat.game
+    @current_round = Helper.current_round(@game)
+  end
+
+
+
+  def reveal_black_card #make sure this returns an ordered list
+    return @current_round.blackcard.content
+  end
+
 end
-
-
-
-
-
-
