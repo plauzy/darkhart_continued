528aaaf32990362b9a812af4d03abdd13d359541
Cassidy Clawson & Patrick Lauer <pair+cassidyclawson.plauer@devbootcamp.com>
2014-09-06 20:05:31 -0700

    snapshot of progress, not tested

--- app/models/game_getter.rb
+++ app/models/game_getter.rb
@@ -9,21 +9,67 @@ class GameGetter
     else
       @round = @game.rounds.where(round_num: round)
     end
+
+    @state = {}
   end
 
-  # def game_state(round = @current_round)
-  #   state = { header: build_header }
-  #   if seat doesn't have a turn.
-  #     state.body = build_no_turn
+  def game_state
+    build_header
+    if round_active?
 
+    elsif !round_active?
+      build_recap
+    end
 
+    return @state
+  end
 
-  #   return state
-  # end
+  private
+
+# Conditionals ----------------
 
   def round_active?
-    # if @current_round.submissions.where(winner: true)
-    # end
+    @round.submissions.where(winner: true)[0] ? false : true
+  end
 
+# Header Methods --------------
+
+  def build_header
+    @state.round = @round.round_num
+    @state.active = round_active?
+    @state.player_self = player_self
+    @state.leader = leader
+  end
+
+  def player_self
+    { name: @seat.user.name,
+      score: @seat.score,
+      seat_id: @seat.id,
+      cards: playable_cards }
+  end
+
+  def playable_cards
+    @seat.playable_cards.where(submitted: false).to_a
+  end
+
+  def leader
+    { leader?: @round.leader_id == @seat.id,
+      blackcard_content: @round.blackcard.content }
+  end
+
+# Recap Methods ----------------
+
+  def recap
+    @state.losing_submissions = []
+    @round.submissions.each |sub|
+      sub_details = { player_name: sub.playable_card.seat.user.name,
+                      player_score: sub.playable_card.seat.score,
+                      submission_content: sub.playable_card.whitecard.content }
+      @state.losing_submissions << sub_details if sub.winner == false
+      @state.winning_submission << sub_details if sub.winner == true
+    end
   end
 end
+
+
+
