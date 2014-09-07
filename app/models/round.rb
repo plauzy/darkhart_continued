6ea125b4e267b50e88244bb5e3f9a0659fc26ab8
Cassidy Clawson & Ian Bui <pair+cassidyclawson.thinkbui@devbootcamp.com>
2014-09-06 19:36:14 -0700

    Added Round tests

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,6 @@ class Round < ActiveRecord::Base
   belongs_to :game
   belongs_to :blackcard
   has_many :submissions
+
+  validates_presence_of :blackcard_id,:game_id,:round_num,:leader_id
 end
--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -6,17 +6,41 @@ RSpec.describe Round, :type => :model do
     Round.destroy_all
     User.destroy_all
     Blackcard.destroy_all
-    game = Game.create()
-    user = User.create(name: "test dummy")
-    Blackcard
+    Whitecard.destroy_all
+    Submission.destroy_all
+    PlayableCard.destroy_all
+    Seat.destroy_all
+    @game = Game.create(name: "Test Game")
+    @user = User.create(name: "test dummy")
+    @seat = Seat.create(user_id:@user.id, game_id: @game.id)
+    @blackcard = Blackcard.create(content:"Test Black Card")
+    @whitecard = Whitecard.create(content:"Test White Card")
+    @playable_card = PlayableCard.create(seat_id: @seat.id,whitecard_id: @whitecard.id)
   end
 
-  it 'Creating a new Round adds a Round object' do
-
-    expect{Round.create(game_id: game.id, )}.to change{Round.count}.by(1)
+  it 'adds a round to the databse' do
+    expect{Round.create(game_id: @game.id, leader_id: @seat.id, blackcard_id: @blackcard.id, round_num: 5783)}.to change{Round.count}.by(1)
   end
 
-      t.integer :leader_id
-      t.integer :round_num
-      t.integer :blackcard_id
+  it 'accepts a submission' do
+    round = Round.create(game_id: @game.id, leader_id: @seat.id, blackcard_id: @blackcard.id, round_num: 5783)
+    expect{Submission.create(round_id: round.id, playable_card_id:@playable_card.id)}.to change{round.submissions.count}.by(1)
+  end
+
+  it 'won\'t create a round without a black card' do
+    expect{Round.create(game_id: @game.id, leader_id: @seat.id,  round_num: 5783)}.to_not change{Round.count}
+  end
+
+  it 'won\'t create a round without a game' do
+    expect{Round.create(leader_id: @seat.id, blackcard_id: @blackcard.id, round_num: 5783)}.to_not change{Round.count}
+  end
+
+  it 'won\'t create a round without a leader' do
+    expect{Round.create(game_id: @game.id, blackcard_id: @blackcard.id, round_num: 5783)}.to_not change{Round.count}
+  end
+
+  it 'won\'t create a round without a round number' do
+    expect{Round.create(game_id: @game.id, leader_id: @seat.id, blackcard_id: @blackcard.id)}.to_not change{Round.count}
+  end
+
 end
