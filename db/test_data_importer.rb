358cdcc6c7f5fa82c3cf314f38e4ae85097fd2f0
Cassidy Clawson <cassidyclawson@gmail.com>
2014-09-07 12:56:44 -0700

    cleaned up but not changed

--- db/test_data_importer.rb
+++ db/test_data_importer.rb
@@ -1,33 +1,3 @@
-# module TestDataImporter
-#   def self.import
-
-#     4.times do
-#       FactoryGirl.create(:user)
-#     end
-
-#     game = FactoryGirl.create(:game)
-#     users = User.all[0..3]
-
-#     whitecards = Whitecard.all.to_a
-#     blackcards = Blackcard.all.to_a
-
-#     #cards dealt to users
-#     4.times do
-#       seat = FactoryGirl.create(:seat, user: users.pop, game: game )
-#       4.times do
-#         FactoryGirl.create(:playable_card, whitecard: whitecards.pop, seat: seat)
-#       end
-#     end
-
-#     #round initiated
-#     round = FactoryGirl.create(:round, game: game, blackcard: blackcards.pop)
-
-
-#   end
-# end
-
-# TestDataImporter.import
-
 module TestDataImporter
   def self.import
 
@@ -53,7 +23,7 @@ module TestDataImporter
 
     4.times do |i|
       leader_id = seats.last.id
-      round = FactoryGirl.create(:round, game: game, blackcard: blackcards.pop, round_num: i+1, leader_id: leader_id)
+      round = FactoryGirl.create(:round, game: game, blackcard: blackcards.pop, round_num: i, leader_id: leader_id)
       submissions = []
 
       3.times do |i|
