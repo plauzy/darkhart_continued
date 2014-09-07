e8fd609c719be9dfd3688936678b217c28fb431f
Cassidy Clawson <cassidyclawson@gmail.com>
2014-09-07 14:19:24 -0700

    snapshot of controller, getter, not returning for previous rounds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
 
   def game_state
+    p "CONTROLLER ------------- #{params["round_num"]}"
     round = params["round_num"] || 0
     game = GameGetter.new(params["game_id"], params["user_id"], round)
     render :json => game.game_state
