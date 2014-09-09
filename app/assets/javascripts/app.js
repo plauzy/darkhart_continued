548c1d557a1d5d385c37c09972adb7835afeb603
Steven G. Harms <steven@devbootcamp.com>
2014-09-09 10:06:53 -0700

    Preliminary move of JS to use new sane routes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,80 +1,89 @@
-$( "#newGameForm" ).submit(function( event ) {
-  event.preventDefault();
-  var $form = $( this ),
-    initiator_id = $form.find( "input[name='initiator_id']" ).val();
-    invite_ids = $form.find( "input[name='invite_ids']" ).val();
-    game_name = $form.find( "input[name='game_name']" ).val();
-    url = "/api/users/" + initiator_id + "/games";
+$(function() {
+  $( "#newGameForm" ).submit(function( event ) {
+    event.preventDefault();
+    var $form = $( this ),
+      initiator_id = $form.find( "input[name='initiator_id']" ).val();
+      invite_ids = $form.find( "input[name='invite_ids']" ).val();
+      game_name = $form.find( "input[name='game_name']" ).val();
+      url = "/api/games/";
 
-    var posting = $.post( url, {  "invite_ids": invite_ids,
-                                  "game_name": game_name } );
-  // Put the results in a div
-  posting.done(function( data ) {
-    $("#json").empty().append(JSON.stringify(data, undefined, 2))
-    console.log(data);
+      debugger
+      var posting = $.post( url, {  "initiator_id": initiator_id,
+                                    "invite_ids": invite_ids,
+                                    "game_name": game_name } );
+    // Put the results in a div
+    posting.done(function( data ) {
+      $("#json").empty().append(JSON.stringify(data, undefined, 2))
+      console.log(data);
+    });
+  });
+
+  $( "#previousRoundRecap" ).submit(function( event ) {
+    event.preventDefault();
+    var $form = $( this ),
+      initiator_id = $form.find( "input[name='initiator_id']" ).val();
+      game_id=  $form.find( "input[name='game_id']" ).val();
+      round_num = $form.find( "input[name='round_num']").val();
+      url = "/api/games/" + game_id + "/rounds/" + round_num;
+      var posting = $.get(url, { "initiator_id": initiator_id });
+
+    // Put the results in a div
+    posting.done(function( data ) {
+      $("#json").empty().append(JSON.stringify(data, undefined, 2))
+      console.log(data);
+    });
+  });
+
+  $( "#currentGameState" ).submit(function( event ) {
+    event.preventDefault();
+    var $form = $( this ),
+      initiator_id = $form.find( "input[name='initiator_id']" ).val();
+      game_id = $form.find( "input[name='game_id']" ).val();
+      url = "/api/games/" + game_id;
+
+      var posting = $.get(url, { "initiator_id": initiator_id });
+    // Put the results in a div
+    posting.done(function( data ) {
+      $("#json").empty().append(JSON.stringify(data, undefined, 2))
+      console.log(data);
+    });
+  });
+
+  $( "#makeSubmission" ).submit(function( event ) {
+    event.preventDefault();
+    var $form = $( this ),
+      initiator_id = $form.find( "input[name='initiator_id']" ).val();
+      game_id = $form.find( "input[name='game_id']" ).val();
+      card_id = $form.find( "input[name='card_id']" ).val();
+      url = "/api/games/" + game_id + "/cards/" + card_id;
+
+      var posting = $.get(url, { "initiator_id": initiator_id });
+    // Put the results in a div
+    posting.done(function( data ) {
+      $("#json").empty().append(JSON.stringify(data, undefined, 2))
+      console.log(data);
+    });
+  });
+
+  $( "#makeDecision" ).submit(function( event ) {
+    event.preventDefault();
+    var $form = $( this ),
+      initiator_id = $form.find( "input[name='initiator_id']" ).val();
+      game_id = $form.find( "input[name='game_id']" ).val();
+      card_id = $form.find( "input[name='submission_id']" ).val();
+      url = "/api/games/" + game_id + "/round/" + round_id; // PATCH to update the round, probably also updates the game as well.
+    var dataBlob = {
+      "initiator_id": initiator_id,
+      "game_id": game_id,
+      "card_id": card_id,
+      "_method": "PATCH"
+    };
+
+    var posting = $.post(url, dataBlob);
+    // Put the results in a div
+    posting.done(function( data ) {
+      $("#json").empty().append(JSON.stringify(data, undefined, 2))
+      console.log(data);
+    });
   });
 });
-
-$( "#previousRoundRecap" ).submit(function( event ) {
-  event.preventDefault();
-  var $form = $( this ),
-    initiator_id = $form.find( "input[name='initiator_id']" ).val();
-    game_id=  $form.find( "input[name='game_id']" ).val();
-    round_num = $form.find( "input[name='round_num']").val();
-    url = "/api/users/" + initiator_id + "/games/" + game_id + "/rounds/" + round_num;
-
-    var posting = $.get( url);
-  // Put the results in a div
-  posting.done(function( data ) {
-    $("#json").empty().append(JSON.stringify(data, undefined, 2))
-    console.log(data);
-  });
-});
-
-$( "#currentGameState" ).submit(function( event ) {
-  event.preventDefault();
-  var $form = $( this ),
-    initiator_id = $form.find( "input[name='initiator_id']" ).val();
-    game_id = $form.find( "input[name='game_id']" ).val();
-    url = "/api/users/" + initiator_id + "/games/" + game_id;
-
-    var posting = $.get( url);
-  // Put the results in a div
-  posting.done(function( data ) {
-    $("#json").empty().append(JSON.stringify(data, undefined, 2))
-    console.log(data);
-  });
-});
-
-$( "#makeSubmission" ).submit(function( event ) {
-  event.preventDefault();
-  var $form = $( this ),
-    initiator_id = $form.find( "input[name='initiator_id']" ).val();
-    game_id = $form.find( "input[name='game_id']" ).val();
-    card_id = $form.find( "input[name='card_id']" ).val();
-    url = "/api/users/" + initiator_id + "/games/" + game_id + "/cards/" + card_id;
-
-    var posting = $.get( url);
-  // Put the results in a div
-  posting.done(function( data ) {
-    $("#json").empty().append(JSON.stringify(data, undefined, 2))
-    console.log(data);
-  });
-});
-
-$( "#makeDecision" ).submit(function( event ) {
-  event.preventDefault();
-  var $form = $( this ),
-    initiator_id = $form.find( "input[name='initiator_id']" ).val();
-    game_id = $form.find( "input[name='game_id']" ).val();
-    card_id = $form.find( "input[name='submission_id']" ).val();
-    url = "/api/users/" + initiator_id + "/games/" + game_id + "/cards/" + card_id;
-
-    var posting = $.get( url);
-  // Put the results in a div
-  posting.done(function( data ) {
-    $("#json").empty().append(JSON.stringify(data, undefined, 2))
-    console.log(data);
-  });
-});
-
