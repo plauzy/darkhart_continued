bc90f58cb0fd9ba0f15d3da8b548da6160ba3e22
plauer <plauer411@gmail.com>
2014-09-11 19:56:13 -0700

    changes choose whitecards to choose card and fized bug with choose white and blackcards

--- app/models/helper.rb
+++ app/models/helper.rb
@@ -1,13 +1,15 @@
 module Helper
   extend self
   def random_whitecard #add validation to prevent duplicate cards and offset is a valid Whitecard
-    offset = rand(1..(Whitecard.count))
-    Whitecard.find(offset)
+    # offset = rand(1..(Whitecard.count))
+    # Whitecard.find(offset)
+    Whitecard.all.sample
   end
 
   def random_blackcard #add validation to prevent duplicate cards and offset is valid blackcard
-    offset = rand(1..(Blackcard.count))
-    Blackcard.find_by_id(offset)
+    # offset = rand(1..(Blackcard.count))
+    # Blackcard.find_by_id(offset)
+    Blackcard.all.sample
   end
 
   def current_round(game)
